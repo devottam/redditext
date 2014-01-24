7c93730f34ff09d9a01928ad7e5fb74bd06f25e2
Devottam Regmi <devottam@gmail.com>
2014-01-24 01:33:32 -0500

    Added files

---
+++ net_helper.go
@@ -0,0 +1,21 @@
+package redditext
+
+import (
+  "bytes"
+  "errors"
+  "net/http"
+)
+
+func Fetch(url string) ([]byte, error) {
+  resp, err := http.Get(url)
+  if err != nil {
+    return nil, err
+  }
+  defer resp.Body.Close()
+  if resp.StatusCode != http.StatusOK {
+    return nil, errors.New(resp.Status)
+  }
+  buffer := new(bytes.Buffer)
+  buffer.ReadFrom(resp.Body)
+  return buffer.Bytes(), nil
+}
